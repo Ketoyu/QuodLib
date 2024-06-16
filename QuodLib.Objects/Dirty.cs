93e9bd2b4438892d1414da5f0e8a1a8ce9e2abe9
Ketoyu <JonathanGramm@Yahoo.com>
2024-06-16 16:36:07 -0500

    Added new items: DIrty, IDirty.

---
+++ QuodLib.Objects/Dirty.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuodLib.Objects {
+    public class Dirty<TData> : IDirty<TData> {
+        private bool _dirty;
+        public bool IsDirty {
+            get => _dirty;
+            set {
+                if (_dirty == value)
+                    return;
+
+                _dirty = value;
+                if (value)
+                    Soil?.Invoke(this, EventArgs.Empty);
+                else
+                    Clean?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public TData Data { get; init; }
+
+        public event EventHandler? Clean;
+
+        public event EventHandler? Soil;
+        public Dirty(TData data) {
+            Data = data;
+        }
+    }
+}
---
+++ QuodLib.Objects/IDirty.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuodLib.Objects {
+    public interface IDirty<TData> {
+        bool IsDirty { get; }
+        TData Data { get; }
+        event EventHandler? Soil;
+        event EventHandler? Clean;
+    }
+}
