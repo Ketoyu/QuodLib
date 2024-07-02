965f4e6f9586aa7627ac77dc21f38c0889dce6c0
Jonathan Gramm <JonathanGramm@Yahoo.com>
2024-07-02 18:06:12 -0500

    added .AsEnumerable() to ListViewItemCollection.

---
+++ QuodLib.WinForms.Linq/Extensions/Collections.cs
@@ -0,0 +1,11 @@
+﻿namespace QuodLib.WinForms.Linq.Extensions {
+    public static class Collections {
+        /// <summary>
+        /// Performs an <see cref="Enumerable.Cast{TResult}(System.Collections.IEnumerable)"/> on the <paramref name="source"/>.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static IEnumerable<ListViewItem> AsEnumerable(this ListView.ListViewItemCollection source)
+            => source.Cast<ListViewItem>();
+    }
+}
