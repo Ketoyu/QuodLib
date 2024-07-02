ecfc8f8f0dff639d95ee5109f965ec1e5a307486
Jonathan Gramm <JonathanGramm@Yahoo.com>
2024-07-01 21:07:05 -0500

    Added delegate-based classes implementing ICommand.

---
+++ QuodLib.Avalonia/DelegateCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Input;
+
+namespace QuodLib.Avalonia {
+    public class DelegateCommand : ICommand {
+        private Func<object?, bool>? _canExecute;
+        private Action<object?> _execute;
+
+        public DelegateCommand(Action<object?> execute, Func<object?, bool> canExecute) {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public DelegateCommand(Action<object?> execute) {
+            _execute = execute;
+            _canExecute = null;
+        }
+
+        public bool CanExecute(object? parameter)
+            => _canExecute?.Invoke(parameter) ?? true;
+
+        public void Execute(object? parameter)
+            => _execute.Invoke(parameter);
+
+        public event EventHandler? CanExecuteChanged;
+    }
+}
---
+++ QuodLib.Avalonia/DelegateCommandAsync.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows.Input;
+
+namespace QuodLib.Avalonia {
+    public class DelegateCommandAsync : ICommand {
+        private Func<object?, bool>? _canExecute;
+        private Func<object?, Task> _execute;
+        private bool _isExecuting;
+
+        public bool IsExecuting {
+            get => _isExecuting;
+            set {
+                _isExecuting = value;
+                CanExecuteChanged?.Invoke(this, new EventArgs());
+            }
+        }
+
+        public DelegateCommandAsync(Func<object?, Task> execute, Func<object?, bool> canExecute) {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public DelegateCommandAsync(Func<object?, Task> execute) {
+            _execute = execute;
+            _canExecute = null;
+        }
+
+        public bool CanExecute(object? parameter)
+            => !IsExecuting && (_canExecute?.Invoke(parameter) ?? true);
+
+        public async void Execute(object? parameter) {
+            IsExecuting = true;
+            await _execute.Invoke(parameter);
+            IsExecuting = false;
+        }
+
+        public void OnCanExecuteChanged() {
+            if (IsExecuting)
+                return;
+
+            CanExecuteChanged?.Invoke(this, new EventArgs());
+        }
+
+        public event EventHandler? CanExecuteChanged;
+    }
+}
