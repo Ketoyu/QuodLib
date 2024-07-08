190e7ec550ca085b721cabd6e2118e2330b431cf
Jonathan Gramm <JonathanGramm@Yahoo.com>
2024-07-07 21:58:58 -0500

    Added documentation, mostly copied from native ICommand.

--- QuodLib.Avalonia/DelegateCommand.cs
+++ QuodLib.Avalonia/DelegateCommand.cs
@@ -1,6 +1,9 @@
 ﻿using System.Windows.Input;
 
 namespace QuodLib.Avalonia {
+    /// <summary>
+    /// Accepts a delegate and uses that for the command.
+    /// </summary>
     public class DelegateCommand : ICommand {
         private Func<object?, bool>? _canExecute;
         private Action<object?> _execute;
@@ -15,12 +18,30 @@ namespace QuodLib.Avalonia {
             _canExecute = null;
         }
 
+        /// <summary>
+        /// Defines the method that determines whether the command can execute in its current state.
+        /// </summary>
+        /// <param name="parameter">
+        ///     Data used by the command. If the command does not require data to be passed, 
+        ///     this object can be set to null.
+        /// </param>
+        /// <returns>true if this command can be executed; otherwise, false.</returns>
         public bool CanExecute(object? parameter)
             => _canExecute?.Invoke(parameter) ?? true;
 
+        /// <summary>
+        /// Defines the method to be called when the command is invoked.
+        /// </summary>
+        /// <param name="parameter">
+        ///     Data used by the command. If the command does not require data to be passed, 
+        ///     this object can be set to null.
+        /// </param>
         public void Execute(object? parameter)
             => _execute.Invoke(parameter);
 
+        /// <summary>
+        /// Occurs when changes occur that affect whether or not the command should execute.
+        /// </summary>
         public event EventHandler? CanExecuteChanged;
     }
 }
--- QuodLib.Avalonia/DelegateCommandAsync.cs
+++ QuodLib.Avalonia/DelegateCommandAsync.cs
@@ -1,6 +1,9 @@
 ﻿using System.Windows.Input;
 
 namespace QuodLib.Avalonia {
+    /// <summary>
+    /// Accepts an async delegate and uses that for the command.
+    /// </summary>
     public class DelegateCommandAsync : ICommand {
         private Func<object?, bool>? _canExecute;
         private Func<object?, Task> _execute;
@@ -25,9 +28,24 @@ namespace QuodLib.Avalonia {
             _canExecute = null;
         }
 
+        /// <summary>
+        /// Defines the method that determines whether the command can execute in its current state.
+        /// </summary>
+        /// <param name="parameter">
+        ///     Data used by the command. If the command does not require data to be passed, 
+        ///     this object can be set to null.
+        /// </param>
+        /// <returns>true if this command can be executed; otherwise, false.</returns>
         public bool CanExecute(object? parameter)
             => !IsExecuting && (_canExecute?.Invoke(parameter) ?? true);
 
+        /// <summary>
+        /// Defines the method to be called when the command is invoked.
+        /// </summary>
+        /// <param name="parameter">
+        ///     Data used by the command. If the command does not require data to be passed, 
+        ///     this object can be set to null.
+        /// </param>
         public async void Execute(object? parameter) {
             IsExecuting = true;
             try {
@@ -45,6 +63,9 @@ namespace QuodLib.Avalonia {
             CanExecuteChanged?.Invoke(this, new EventArgs());
         }
 
+        /// <summary>
+        /// Occurs when changes occur that affect whether or not the command should execute.
+        /// </summary>
         public event EventHandler? CanExecuteChanged;
     }
 }
