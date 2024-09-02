8ee5d32df41dd8a088e5dd05464da684bab79214
Ketoyu <jonathangramm@yahoo.com>
2024-09-02 16:54:33 -0500

    Added an empty implementation of IProgress<T>, for passing to methods that require IProgress<T> parameters.

---
+++ QuodLib.Objects/EmptyProgress.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuodLib.Objects
+{
+    /// <summary>
+    /// An empty implementation of <see cref="IProgress{T}"/>; see <see cref="Value"/>.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed class EmptyProgress<T> : IProgress<T> {
+        /// <summary>
+        /// An empty <see cref="IProgress{T}"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public static readonly EmptyProgress<T> Value = new();
+
+        /// <summary>
+        /// Empty implementation; does nothing.
+        /// </summary>
+        /// <param name="value"></param>
+        public void Report(T value)
+        { } //Do nothing
+    }
+}
