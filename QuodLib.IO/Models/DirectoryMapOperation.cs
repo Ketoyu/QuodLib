62185c9c0d0e546c179f49745e16189944cf1cbb
Ketoyu <jonathangramm@yahoo.com>
2024-09-03 21:45:02 -0500

    Moved in more file-copying code from the SysBackup solution.

---
+++ QuodLib.IO/Models/DirectoryMapOperation.cs
@@ -0,0 +1,11 @@
+﻿namespace QuodLib.IO.Models {
+    /// <summary>
+    /// Pending instruction to create a target directory based on a source directory.
+    /// </summary>
+    public sealed class DirectoryMapOperation : DirectoryCreateOperation {
+        /// <summary>
+        /// The source directory which was used to generate the target path.
+        /// </summary>
+        public required string SourcePath { get; init; }
+    }
+}
---
+++ QuodLib.IO/Models/IOBulkOperation.cs
@@ -0,0 +1,72 @@
+﻿namespace QuodLib.IO.Models {
+    /// <summary>
+    /// A collection of <see cref="IOOperation"/>s to be run.
+    /// </summary>
+    public sealed class IOBulkOperation {
+        /// <summary>
+        /// The list of operations to be performed.
+        /// </summary>
+        public required IList<IOOperation> Operations { get; init; }
+
+        /// <summary>
+        /// The total size of source files to be operated on.
+        /// </summary>
+        public required long Size { get; init; }
+
+        /// <summary>
+        /// The total count of source files to be operated on.
+        /// </summary>
+        public required long Count { get; init; }
+
+        /// <summary>
+        /// Run the full list of <see cref="Operations"/>.
+        /// </summary>
+        /// <param name="progress"></param>
+        /// <param name="error"></param>
+        /// <param name="cancel"></param>
+        /// <returns></returns>
+        /// <exception cref="NotSupportedException"></exception>
+        public async Task RunParallelAsync(IProgress<IOProgressModel?> progress, IProgress<IOErrorModel> error, CancellationToken cancel) {
+            long sizeDestination = 0;
+            long countDestination = 0;
+            IProgress<long> pDest = new Progress<long>().OnChange((_, add) => {
+                sizeDestination += add;
+                countDestination++;
+            });
+
+            IProgress<bool> pProg = new Progress<bool>().OnChange((_, success) => {
+                progress.Report(new IOProgressModel {
+                    SourceSize = Size,
+                    SourceCount = Count,
+                    CurrentSize = sizeDestination,
+                    CurrentCount = countDestination,
+                    Success = success
+                });
+            });
+
+            //Copy folders & files
+            await Parallel.ForEachAsync(Operations.ToArray(), cancel, (itm, pcancel) => {
+                try {
+                    itm.Run();
+                    if (itm is FileOperation opFile) {
+                        pDest.Report(new FileInfo(opFile.SourcePath).Length);
+                        //status.Report(new($"Copying: {cF.Filename_GetPath(itm)}", true));
+                    }
+
+                    pProg.Report(true);
+                } catch (Exception ex) {
+                    if (itm is DirectoryMapOperation opDir)
+                        error.Report(new(PathType.Folder, opDir.SourcePath, ex));
+                    else if (itm is FileOperation opFile)
+                        error.Report(new(PathType.File, opFile.SourcePath, ex));
+                    else
+                        throw new NotSupportedException(itm.GetType().Name);
+
+                    pProg.Report(false);
+                }
+
+                return ValueTask.CompletedTask;
+            });
+        }
+    }
+}
