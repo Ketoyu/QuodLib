aae16510f2ac87696f72c6a37fc3bdc14ec206f2
Ketoyu <jonathangramm@yahoo.com>
2024-09-18 13:13:15 -0500

    WinForms.Linq: add fluid extension-method for adding sub-items to a `ToolStripMenuItem`.

---
+++ QuodLib.WinForms.Linq/Extensions/Nested.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuodLib.WinForms.Linq.Extensions {
+    public static class Nested {
+        /// <summary>
+        /// A fluid method for adding sub-items to a <see cref="ToolStripMenuItem"/>.
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="items"></param>
+        /// <remarks>
+        /// <list type="bullet">
+        ///     <item>See also <see cref="ToolStripMenuItem"/></item>
+        ///     <item>See also <see cref="ToolStripDropDownItem.DropDownItems"/></item>
+        ///     <item>See also <see cref="ToolStripItemCollection.AddRange(ToolStripItem[])"/></item>
+        /// </list>
+        /// </remarks>
+        /// <returns></returns>
+        public static ToolStripMenuItem AddSubitems(this ToolStripMenuItem parent, params ToolStripMenuItem[] items) {
+            parent.DropDownItems.AddRange(items);
+            return parent;
+        }
+    }
+}
