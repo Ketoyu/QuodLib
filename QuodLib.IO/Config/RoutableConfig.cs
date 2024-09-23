0588a0e5ee9a2727ae5de641fe39b00a0ff795fc
Ketoyu <jonathangramm@yahoo.com>
2024-09-23 18:11:12 -0500

    RoutableConfig: minor overhaul, and added more XML-comments.

--- QuodLib.IO/Config/RoutableConfig.cs
+++ QuodLib.IO/Config/RoutableConfig.cs
@@ -1,4 +1,5 @@
-﻿using IODirectory = System.IO.Directory;
+﻿using System.IO;
+using IODirectory = System.IO.Directory;
 using IOFile = System.IO.File;
 using IOPath = System.IO.Path;
 
@@ -8,34 +9,107 @@ namespace QuodLib.IO.Config {
     /// outputs the effective directory to <see cref="Directory"/>. Creates the file if it does not exist.
     /// </summary>
     public class RoutableConfig {
+        /// <summary>
+        /// The effective directory, after routing has been processed.
+        /// </summary>
         public string Directory { get; protected init; }
 
-        public RoutableConfig(string publisher, string programName) {
-            string path = IOPath.Combine(
-                    Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
-                    string.Join('_', publisher.Split(IOPath.GetInvalidPathChars()))
+        /// <summary>
+        /// Rerouts via <code>$"{<paramref name="directory"/>}\reroute.ini"</code>
+        /// </summary>
+        /// <param name="directory"></param>
+        /// <remarks>
+        ///     Replaces invalid characters with underscores.<br />
+        ///     See also <see cref="IOPath.GetInvalidPathChars"/>
+        /// </remarks>
+        public RoutableConfig(string directory) {
+            string file = IOPath.Combine(directory, @"reroute.ini");
+
+            bool exists = IOFile.Exists(file);
+
+            if (exists && Ini.Load(file).TryGetValue("Path", out string? dir_) && !string.IsNullOrEmpty(dir_)) {
+                IODirectory.CreateDirectory(dir_);
+                Directory = dir_;
+                return;
+            }
+
+            if (!exists)
+                Ini.Save(
+                    new() { { "Path", directory } },
+                    file
                 );
 
-            string file = IOPath.Combine(path, @"reroute.ini");
-
-            if (!IOFile.Exists(file)) {
-                Ini.Save(new Dictionary<string, string>() {
-                        {
-                            "Path",
-                            path
-                        }
-                    }, file);
-                Directory = IOPath.Combine(path, programName);
-                return;
-            }
-
-            if (Ini.Load(file).TryGetValue("Path", out string? path_) && !string.IsNullOrEmpty(path_)) {
-                IODirectory.CreateDirectory(path_);
-                Directory = IOPath.Combine(path_, programName);
-                return;
-            }
-
-            Directory = IOPath.Combine(path, programName);
+            Directory = directory;
         }
+
+        /// <summary>
+        /// Rerouts via <code>$"{MyDocuments}\{<paramref name="publisher"/>}\reroute.ini"</code> and sets the <see cref="Directory"/> to <code>$"{routedDirectory}\{<paramref name="programName"/>}"</code>
+        /// </summary>
+        /// <param name="publisher"></param>
+        /// <param name="programName"></param>
+        /// <remarks>
+        ///     Replaces invalid characters with underscores.
+        ///     <list type="bullet">
+        ///         <item>See also <see cref="Environment.SpecialFolder.MyDocuments"/></item>
+        ///         <item>See also <see cref="IOPath.GetInvalidPathChars"/></item>
+        ///     </list>
+        /// </remarks>
+        protected RoutableConfig(string publisher, string programName) : this(MyDocuments(publisher)) {
+            //At this point Directory is "...\Documents\{publisher}\reroute.ini"
+
+            Directory = IOPath.Combine(
+                Directory, 
+                string.Join('_', programName.Split(IOPath.GetInvalidPathChars()))
+            );
+        }
+
+        /// <summary>
+        /// Rerouts via <code>$"{MyDocuments}\{<paramref name="folderName"/>}\reroute.ini"</code>
+        /// </summary>
+        /// <param name="folderName"></param>
+        /// <returns></returns>
+        /// <remarks>
+        ///     Replaces invalid characters with underscores.
+        ///     <list type="bullet">
+        ///         <item>See also <see cref="Environment.SpecialFolder.MyDocuments"/></item>
+        ///         <item>See also <see cref="IOPath.GetInvalidPathChars"/></item>
+        ///     </list>
+        /// </remarks>
+        public static RoutableConfig FromMyDocuments(string folderName)
+            => new(MyDocuments(folderName));
+
+        /// <summary>
+        /// Rerouts via <code>$"{MyDocuments}\{<paramref name="publisher"/>}\reroute.ini"</code> and sets the <see cref="Directory"/> to <code>$"{routedDirectory}\{<paramref name="programName"/>}"</code>
+        /// </summary>
+        /// <param name="publisher"></param>
+        /// <param name="programName"></param>
+        /// <returns></returns>
+        /// <remarks>
+        ///     Replaces invalid characters with underscores.
+        ///     <list type="bullet">
+        ///         <item>See also <see cref="Environment.SpecialFolder.MyDocuments"/></item>
+        ///         <item>See also <see cref="IOPath.GetInvalidPathChars"/></item>
+        ///     </list>
+        /// </remarks>
+        public static RoutableConfig FromMyDocuments(string publisher, string programName)
+            => new(publisher, programName);
+
+        /// <summary>
+        /// Returns <code>$"{MyDocuments}\{<paramref name="folderName"/>}"</code>
+        /// </summary>
+        /// <param name="folderName"></param>
+        /// <returns></returns>
+        /// <remarks>
+        ///     Replaces invalid characters with underscores.
+        ///     <list type="bullet">
+        ///         <item>See also <see cref="Environment.SpecialFolder.MyDocuments"/></item>
+        ///         <item>See also <see cref="IOPath.GetInvalidPathChars"/></item>
+        ///     </list>
+        /// </remarks>
+        protected static string MyDocuments(string folderName)
+            => IOPath.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                    string.Join('_', folderName.Split(IOPath.GetInvalidPathChars()))
+                );
     }
 }
