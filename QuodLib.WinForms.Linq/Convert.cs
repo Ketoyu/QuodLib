1e99bdd2b32c231498dcce71feee215bbf565d83
Ketoyu <jonathangramm@yahoo.com>
2024-09-24 16:28:00 -0500

    add extension for casting ComboBox.SelectedItem

--- QuodLib.WinForms.Linq/Convert.cs
+++ QuodLib.WinForms.Linq/Convert.cs
@@ -9,5 +9,13 @@
         /// <returns></returns>
         public static IEnumerable<ListViewItem> ToEnumerable(this ListView.ListViewItemCollection source)
             => source.Cast<ListViewItem>();
+
+        /// <summary>
+        /// Casts the <paramref name="source"/>'s <see cref="ComboBox.SelectedItem"/> to the provided <typeparamref name="T"/>?
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static T? SelectedItem<T>(this ComboBox source)
+            => (T?)source.SelectedItem;
     }
 }
