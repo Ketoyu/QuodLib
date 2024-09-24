6ba5a43c2edb968dd401263a6f000a670275b55a
Ketoyu <jonathangramm@yahoo.com>
2024-09-24 16:35:10 -0500

    1. Rename "AsEnumerable()" to "ToEnumerable()" to match industry convention, 2. rename "As.cs" to "Convert.cs"

--- QuodLib.Linq/Convert.cs
+++ QuodLib.Linq/Convert.cs
@@ -1,12 +1,12 @@
 ﻿namespace QuodLib.Linq {
-    public static class As {
+    public static class Convert {
 
-        public static IEnumerable<int> AsEnumerable(this Range range) {
+        public static IEnumerable<int> ToEnumerable(this Range range) {
             for (int i = range.Start.Value; i <= range.End.Value; i++)
                 yield return i;
         }
 
-        public static IEnumerable<IEnumerable<T>> AsEnumerable<T>(this T[,] source) {
+        public static IEnumerable<IEnumerable<T>> ToEnumerable<T>(this T[,] source) {
             for (int i = 0; i < source.Length; i++)
                 yield return source.At(i);
         }
