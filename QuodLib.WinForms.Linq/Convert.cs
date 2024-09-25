1b7ec104be3b2ab098eaf562cefd6a6036d28c70
Ketoyu <jonathangramm@yahoo.com>
2024-09-24 23:52:49 -0500

    Add casting extensons for `ListBox.SelectedItem` and `ListBox.SelectedItems`

--- QuodLib.WinForms.Linq/Convert.cs
+++ QuodLib.WinForms.Linq/Convert.cs
@@ -17,5 +17,30 @@
         /// <returns></returns>
         public static T? SelectedItem<T>(this ComboBox source)
             => (T?)source.SelectedItem;
+
+        /// <summary>
+        /// Casts the <paramref name="source"/>'s <see cref="ListBox.SelectedItem"/> to the provided <typeparamref name="T"/>?
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static T? SelectedItem<T>(this ListBox source)
+            => (T?)source.SelectedItem;
+
+        /// <summary>
+        /// Casts the <paramref name="source"/>'s <see cref="ListBox.SelectedItems"/> to the provided <typeparamref name="T"/>
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static T[] SelectedItems<T>(this ListBox source) {
+            if (source.SelectedItems.Count == 0)
+                return Array.Empty<T>();
+
+            T[] output = new T[source.SelectedItems.Count];
+            for (int i = 0; i < source.SelectedItems.Count; i++)
+                output[i] = (T)source.SelectedItems[i]!;
+
+            return output;
+        }
+            
     }
 }
